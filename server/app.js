c0ea79edc8c0f6db22adc1d8e2bc45c97f76393b
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2024-10-10 11:20:31 +0530

    adding ip retrieval

--- server/app.js
+++ server/app.js
@@ -20,9 +20,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./router/auth'));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
-});
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public/index.html'));
+// });
 
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running at port ${PORT}`);
